26f3ee111051d290d18356813215e157ab5d4364
Christian Weiske <cweiske@cweiske.de>
2012-11-13 19:10:13 +0100

    add fixme

--- src/PEAR2/Services/Pingback2/Server/Callback/FetchSource.php
+++ src/PEAR2/Services/Pingback2/Server/Callback/FetchSource.php
@@ -20,6 +20,12 @@ class Server_Callback_FetchSource implements Server_Callback_ISource
             . ', application/xhtml+xml;q=0.9'
             . ', */*;q=0.1'
         );
+        /* FIXME: add content range to respect:
+          In order to avoid susceptibility to denial of service attacks,
+          pingback servers that fetch the specified source document
+          (as described in section 3) are urged to impose limits on the
+          size of the source document to be examined and the rate of data
+          transfer.  */
         return $req->send();
     }
 }
